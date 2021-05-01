fd04ce28f71f736533bc80baf6a6d01470601ca0
Rushil Perera <pererarushil1940@gmail.com>
2021-05-01 12:26:19 -0400

    feat: added logs to help with debugging issues

---
+++ logger.ts
@@ -0,0 +1,25 @@
+import dayjs from "dayjs";
+import { createWriteStream } from "fs";
+import pino from "pino";
+import pinoMulti from "pino-multi-stream";
+
+const prettyStream = pinoMulti.prettyStream({
+  prettyPrint: {
+    ignore: "time",
+  },
+});
+
+export const logger = pino(
+  {
+    level: "trace",
+  },
+  pinoMulti.multistream([
+    { stream: prettyStream, level: "info" },
+    {
+      stream: createWriteStream(
+        `./drivetest-${dayjs().format("YYYY-MM-DD_HH-mm-ss")}.log`
+      ),
+      level: "trace",
+    },
+  ])
+);
